a6b6020a210a19195d3275a55eb8a5a63788ad45
ne0N322 <msh230309@gmail.com>
2023-11-03 15:50:54 +0200

    dz4

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("Hello")
+class Student():
+    happy = 20
+    def __init__(self, name, hieght = 160):
+        self.name = name
+        self.hieght = hieght
+        Student.happy = 10
+        self.happy = Student.happy
+    def show(self):
+        print(self.name, self.hieght, self.happy)
+
+
+print()
+st1 = Student("Bob", 185)
+st2 = Student("Kat")
+st3 = Student("Pit")
+
+st1.show()
+st2.show()
