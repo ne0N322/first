795fde51cbc626410b9e6dde735adc5a0ac57d7d
ne0N322 <msh230309@gmail.com>
2023-12-01 16:57:11 +0200

    dzfn

--- hltv.py
+++ hltv.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+# import sqlite3
 
-response = requests.get("https://www.hltv.org/")
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+response = requests.get("https://www.hltv.org/", headers=headers)
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, features="html.parser")
     newshltv = soup.find_all("div", {"class": "standard-box standard-list"})
     if newshltv:
-        newsline = newshltv[0].find("div", {"class": "newsline article"})
-        newstext = newshltv[0].find("div", {"class": "newstext"})
+        newsline = soup.find_all("div", {"class": "newsline article"})
+        newstext = soup.find_all("div", {"class": "newstext"})
         if newstext:
-            print(newstext.text)
+            for news in newstext:
+                news_text = news.get_text(strip=True)
+                print(news_text)
         else:
             print('-')
     else:
